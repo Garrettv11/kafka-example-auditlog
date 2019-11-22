a6615898eea322a3371d22c0a3c8ff031103c5ce
Garrett Kim <garrett.kim@C02VK0JWHTDD-garrett.kim>
2019-11-22 13:44:14 -0800

    [XX-000] added sample unit test

---
+++ test/unit/sample.test.js
@@ -0,0 +1,11 @@
+
+
+beforeAll(async () => {
+  // setup before each test
+});
+
+describe('Example unit test', () => {
+  test('example unit test', async () => {
+    expect(true).toBe(true);
+  });
+});
