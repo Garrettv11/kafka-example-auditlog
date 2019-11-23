3bbf28cd9c0f6c5d04e537ade6b2edb5495b8420
Garrett Kim <garrett.kim@science37.com>
2019-11-23 01:08:03 -0800

    [XX-000] made example audit package json

---
+++ src/consumers/AuditLogConsumer.js
@@ -0,0 +1,51 @@
+const kafka = require('kafka-node');
+const Promise = require('bluebird');
+// const ElasticSearchDao = require(__dirname + '/../dao/ElasticSearch.js');
+const TOPIC_AUDITLOG_CREATE = 'AuditLogCreate';
+// const config = require(__dirname + '/../../config.js');
+
+/**
+ * @classdesc Form Producer that pushes updates to Kafka.
+ * @class
+ */
+class AuditLogCreateConsumer {
+  /**
+  * Create FormCreateConsumer.
+  * @constructor
+  * @param {String} kafkaHost - address of kafka server
+  */
+  constructor(kafkaHost) {
+    const client = new kafka.KafkaClient({kafkaHost});
+    const Consumer = kafka.Consumer;
+    this.consumer = Promise.promisifyAll(new Consumer(
+      client,
+      [ { topic: TOPIC_AUDITLOG_CREATE } ],
+      {
+        autoCommit: false,
+      },
+    ));
+
+    this.consumer.on('message', async message => {
+      // try to store the form in S3
+      const auditMessage = JSON.parse(message.value);
+      console.log('the message being processed is :', JSON.stringify(auditMessage));
+
+      try {
+        // we need to diff these records
+        // use the docs/exampleAuditEntry.json
+        // await this.consumer.commitAsync();
+      }
+      catch (error) {
+        console.log('error processing form:', error);
+        throw error;
+      }
+    });
+
+    this.consumer.on('error', err => {
+      console.log('Form Create Producer error is :', err);
+      throw err;
+    });
+  }
+}
+
+module.exports = AuditLogCreateConsumer;
